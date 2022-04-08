f86adfa1ed82d6fe733cd781c9aabf348ea4441a
mekb the turtle <mekbgithub@icloud.com>
2022-04-08 12:45:05 +1000

    changes

--- index.js
+++ index.js
@@ -7,18 +7,24 @@ const { spawn } = require("child_process");
 let unknown;
 let opt = {
 	string: [ "file", "output", "expiry" ],
-	boolean: [ "help", "random", "reverse", "dither", "ignore", "quiet", "debug" ],
+	boolean: [ "help", "random", "reverse", "dither", "white", "ignore", "quiet", "debug", "noexisting" ],
 	alias: {
 		"help": [ "?" ],
 		"file": [ "f" ],
 //		"width": [ "w" ], // resizing currently doesn't work
 //		"height": [ "h" ],
+		"ewidth": [ "W" ],
+		"eheight": [ "H" ],
+		"ex": [ "X" ],
+		"ey": [ "Y" ],
+		"noexisting": [ "O" ],
 		"dither": [ "d" ],
 		"x": [ "x_" ], // this is required otherwise it'll think this is unknown
 		"y": [ "y_" ],
 		"random": [ "r" ],
 		"reverse": [ "R" ],
-		"ignore": [ "i" ],
+		"white": [ "i" ],
+		"ignore": [ "I" ],
 		"debug": [ "D" ],
 		"quiet": [ "q" ],
 		"output": [ "o" ],
@@ -28,25 +34,30 @@ let opt = {
 };
 const argv = require("minimist")(process.argv.splice(2), opt);
 if (argv.help) {
-	console.error("required:");
-	console.error("  --file -f       what file to draw");
-	console.error("  -x              left-most pixel of the image");
-	console.error("  -y              top-most pixel of the image");
-	console.error("use -x-5 or -x=-5 for negative numbers, -x -5 won't work");
 	console.error("image:");
-//	console.error("  --width -w      width of image");
-//	console.error("  --height -h     height of image");
+	console.error("  --file -f         what file to draw");
+	console.error("  -x                left-most pixel of the image");
+	console.error("  -y                top-most pixel of the image");
+	console.error("use -x-5 or -x=-5 for negative numbers, -x -5 won't work");
+//	console.error("  --width -w        width of image");
+//	console.error("  --height -h       height of image");
 //	console.error("if both width and height are left out, image size will be left as is");
-	console.error("  --random -r     draw each pixel in a random order");
-	console.error("  --reverse -R    reverse order to draw pixels");
-	console.error("  --dither -d     dither the image");
-	console.error("  --ignore -i     don't draw white pixels, act as if all pixels are white by default");
+	console.error("  --random -r       draw each pixel in a random order");
+	console.error("  --reverse -R      reverse order to draw pixels");
+	console.error("  --dither -d       dither the image");
+	console.error("  --white -i        don't draw white pixels, act as if all pixels are white by default");
+	console.error("  --ignore -I       don't check existing pixels, makes --expiry useless");
+	console.error("  --expiry -e       expiry time of chunk cache for existing pixels, default: 1h");
 	console.error("output:");
-	console.error("  --debug -D      don't actually draw anything, just say what would be drawn");
-	console.error("  --quiet -q      don't output anything, overrides --debug");
-	console.error("  --output -o     output image of what would be drawn to a file");
-	console.error("  --expiry -e     expiry time of cache, default: 1h");
-	console.error("  --help -?       help");
+	console.error("  --debug -D        don't actually draw anything, just say what would be drawn unless --quiet is specified");
+	console.error("  --quiet -q        don't output anything, does nothing if --debug is specified and not --output");
+	console.error("  --output -o       output image of what would be drawn to a file, use with -D and -q");
+	console.error("  --noexisting -O   don't show existing pixels with -o, defaults to yes if -x and -y are left out");
+	console.error("  --ex -X           left-most pixel for --output");
+	console.error("  --ey -Y           top-most pixel for --output");
+	console.error("  --ewidth -W       width for --output");
+	console.error("  --eheight -H      height for --output");
+	console.error("  --help -?         help");
 	console.error("");
 	return 8;
 }
@@ -55,6 +66,12 @@ if (unknown) {
 	console.error("unexpected", unknown);
 	return 7;
 }
+for (let i = 0; i < opt.boolean.length; ++i) {
+	if (typeof argv[opt.boolean[i]] != "boolean" && argv[opt.boolean[i]] != null) {
+		console.error("wrong type -" + (opt.boolean[i].length > 1 ? "-" : "") + opt.boolean[i]);
+		return 7;
+	}
+}
 const isStr = (e, a) => {
 	if (typeof e != "string" || e == null || e == "") {
 		console.error(`missing ${a}`);
@@ -62,27 +79,46 @@ const isStr = (e, a) => {
 	}
 }
 const isNum = (e, a, p) => {
-	if (typeof e != "number" || e != Math.floor(e) && e >= 1e9 && e <= (p ? 0 : -1e9)) {
+	if (typeof e != "number" || e != Math.floor(e) || e >= 1e9 || e <= (p ? 0 : -1e9)) {
 		console.error(e == null ? `missing ${a}` : `invalid ${a}`);
 		return true;
 	}
 }
-if (argv.output != null) {
-	if (isStr(argv.output, "--output")) return 7;
-}
-if (!argv.debug || !argv.quiet) {
-	if (argv.x == null && argv.y == null && argv.debug)
-		argv.x = argv.y = 0;
+if (argv.output != null) if (isStr(argv.output, "--output")) return 7;
+if (!argv.debug || argv.file != null) if (isStr(argv.file, "--file")) return 7;
+if ((argv.file && !argv.debug) || argv.x != null || argv.y != null) {
 	if (isNum(argv.x, "-x")) return 7;
 	if (isNum(argv.y, "-y")) return 7;
 	if (argv.width != null || argv.height != null) {
 		if (isNum(argv.width,  "--width", true))  return 7;
 		if (isNum(argv.height, "--height", true)) return 7;
 	}
-} else {
-	argv.x = argv.y = argv.width = argv.height = null;
 }
+let isE = false;
+if (argv.output) {
+	if (argv.ewidth != null || argv.eheight != null || argv.ex != null || argv.ey != null) {
+		if (argv.existing) {
+			console.error("--existing specified without --file"); return 7;
+		}
+		if (isNum(argv.ex,      "-X"))              return 7;
+		if (isNum(argv.ey,      "-Y"))              return 7;
+		if (isNum(argv.ewidth,  "--ewidth", true))  return 7;
+		if (isNum(argv.eheight, "--eheight", true)) return 7;
+		isE = true;
+	} else if (!argv.file) {
+		console.error("no --file or -WHXY argumets specified"); return 7;
+	}
+} else {
+	if (argv.ewidth != null)  console.error("--ewidth ignored");
+	if (argv.eheight != null) console.error("--eheight ignored");
+	if (argv.ex != null)      console.error("-X ignored");
+	if (argv.ey != null)      console.error("-Y ignored");
+	if (argv.noexisting)      console.error("--noexisting ignored");
+}
+if (argv.ignore && argv.expiry) console.error("--expiry ignored");
 if (argv.expiry == null) argv.expiry = "1h";
+if (argv.output != null && (argv.x == null || argv.y == null)) { console.error("--noexisting enabled as -x and -y are left out"); argv.noexisting = true; }
+else if (argv.debug && argv.noexisting && (argv.x != null || argv.y != null)) console.error("-x -y ignored");
 if (isStr(argv.expiry, "--expiry")) return 7;
 let expiryMatch = argv.expiry.match(/^(?:([0-6])d)?(?:([0-9]|1[0-9]|2[0-3])h)?(?:([0-9]|[1-5][0-9])m)?(?:([0-9]|[1-5][0-9])s)?$/i);
 if (!expiryMatch) { console.error("invalid --expiry"); return 7; }
@@ -92,8 +128,8 @@ if (expiryMatch[2]) expiryTime += parseInt(expiryMatch[2]) * 3600e3;
 if (expiryMatch[3]) expiryTime += parseInt(expiryMatch[3]) * 60e3;
 if (expiryMatch[4]) expiryTime += parseInt(expiryMatch[4]) * 1e3;
 delete expiryMatch;
-if (isStr(argv.file, "--file")) return 7;
-if (argv.debug && argv.quiet && !argv.output) return 0;
+if (argv.debug && argv.quiet && !argv.output) { console.error("nothing will happen"); return 0; }
+if (!argv.x) argv.x = 0; if (!argv.y) argv.y = 0;
 const paletteFile = path.resolve(__dirname, "./palette.png"); // read palette image
 const palette = await Jimp.read(paletteFile);
 let colors_ = [];
@@ -112,7 +148,6 @@ const ax = axios.create({
 	baseURL: "https://pixelcanvas.io/api/",
 	timeout: 10000,
 	headers: `User-Agent: Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0
-X-Firebase-AppCheck: ${process.env.FIREBASE}
 Origin: https://pixelcanvas.io
 Referer: https://pixelcanvas.io
 DNT: 1
@@ -199,6 +234,9 @@ const drawPixel = async ({ x, y, color }) => {
 	let res = await ax({
 		method: "post",
 		url: "pixel",
+		headers: {
+			"X-Firebase-AppCheck": process.env.FIREBASE
+		},
 		data: {
 			x, y, color,
 			fingerprint: process.env.FINGERPRINT,
@@ -222,8 +260,6 @@ const nearest = (r, g, b) => { // get nearest color
 	}
 	return j;
 };
-if (!process.env.FIREBASE) { console.error("missing env FIREBASE"); return 3; }
-if (!process.env.FINGERPRINT) { console.error("missing env FINGERPRINT"); return 3; }
 let image;
 if ((argv.width && argv.height) || argv.dither) {
 	const buf = await (await Jimp.read(argv.file)).getBufferAsync("image/png");
@@ -269,15 +305,43 @@ image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, i) => {
 	} else {
 		r = g = b = a = 0;
 	}
+	// set color to the nearest
 	image.bitmap.data[i+0] = r;
 	image.bitmap.data[i+1] = g;
 	image.bitmap.data[i+2] = b;
 	image.bitmap.data[i+3] = a;
 });
 if (argv.output) {
-	await fs.promises.writeFile(argv.output, await image.getBufferAsync("image/png"));
+	if (argv.noexisting) {
+		// just write the image if don't need to change the background
+		await fs.promises.writeFile(argv.output, await image.getBufferAsync("image/png"));
+	} else {
+		// get position and size
+		let ex = isE ? argv.ex : argv.x;
+		let ey = isE ? argv.ey : argv.y;
+		let ewidth = isE ? argv.ewidth : image.bitmap.width;
+		let eheight = isE ? argv.eheight : image.bitmap.height;
+		image2 = await Jimp.create(ewidth, eheight);
+		for (let y = 0; y < eheight; ++y) {
+			for (let x = 0; x < ewidth; ++x) {
+				// new color
+				let ox = ex-argv.x+x;
+				let oy = ey-argv.y+y;
+				let oox = ox<0 || ox>=image.bitmap.width;
+				let ooy = oy<0 || oy>=image.bitmap.height;
+				let oc = oox || ooy ? 0 : image.getPixelColor(ox, oy);
+				if (oc & 0xff > 127) {
+					image2.setPixelColor(oc, x, y);
+				} else {
+					// original color from pixelcanvas
+					let c = await getPixel(x+ex, y+ey);
+					image2.setPixelColor(Jimp.rgbaToInt(...colors[c], 255), x, y);
+				}
+			}
+		}
+		await fs.promises.writeFile(argv.output, await image2.getBufferAsync("image/png"));
+	}
 }
-if (argv.debug && argv.quiet) return 0;
 let pixels = [];
 image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, i) => {
 	let [ r, g, b, a ] = image.bitmap.data.slice(i, i+4);
@@ -287,7 +351,7 @@ image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, i) => {
 		// don't need to use nearest twice
 		let color = colors.map(e => e.join()).indexOf([r, g, b].join());
 		// indexOf doesn't work with an array of arrays, but array of strings work
-		if (color == 0 && argv.ignore) return; 
+		if (color == 0 && argv.white) return; 
 		pixels.push({ x: X, y: Y, color });
 	}
 });
@@ -297,6 +361,10 @@ if (argv.reverse) pixels.reverse(); // reverse array
 for (let i = 0; i < pixels.length; ++i) {
 	while (true) {
 		try {
+			if (!argv.debug) {
+				if (!process.env.FIREBASE) { console.error("missing env FIREBASE"); return 3; }
+				if (!process.env.FINGERPRINT) { console.error("missing env FINGERPRINT"); return 3; }
+			}
 			let res = await drawPixel(pixels[i]);
 			if (argv.debug) break; // don't sleep with --debug
 			await sleep(Math.floor(res.waitSeconds * 1e3), i+1, pixels.length);
